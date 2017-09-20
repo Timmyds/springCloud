4865c9e2457540ecd2381420902a057145786df7
Timmyds <296904320@qq.com>
2017-09-20 18:43:02 +0800

    springcloud学习

---
+++ springcloud/springFeign/src/main/java/com/ksg/controller/HiController.java
@@ -0,0 +1,23 @@
+package com.ksg.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ksg.service.SchedualServiceHi;
+
+
+/**
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
